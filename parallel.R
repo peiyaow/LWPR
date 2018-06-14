821a4cd85c9ed5e47f070f909c79df3eba34ec03
peiyaow <monicawang76@gmail.com>
2018-06-14 00:03:30 -0400

    parallel

--- parallel.R
+++ parallel.R
@@ -2,19 +2,22 @@ nsim = 50
 
 # myseeds = floor(1e4 * runif(nsim))
 # save(myseeds, file = "myseed1.RData")
-# load("/netscr/peiyao/np/myseed1.RData")
-load("/netscr/peiyao/myseed1.RData")
+load("/nas/longleaf/home/peiyao/LWPR/myseed1.RData")
 
-for (i in 1:nsim){
-   for (a in c(0, 0.5, 1)){
-      for (t in 1:3){
-        system(paste0('bsub -q week -o SlRfnoDb.txt -M 20 Rscript main_SlRfnoDb.R alpha0=', 0, ' alpha=', a, ' myseed=', myseeds[i], ' t=', t))
-      }
-   }
-}
-
-# 
-# 
 # for (i in 1:nsim){
-#   system(paste0('bsub -q week -o SlRfnoDb.txt -M 20 Rscript main_SlRfnoDb.R myseed=', myseeds[i]))
+#    for (a in c(0, 0.5, 1)){
+#       for (t in 1:3){
+#         system(paste0('bsub -q week -o SlRfnoDb.txt -M 20 Rscript test0613.R alpha0=', 0, ' alpha=', a, ' myseed=', myseeds[i], ' t=', t))
+#       }
+#    }
 # }
+
+a = 0
+t = 1
+for (i in 1:nsim){
+#  for (a in c(0, 0.5, 1)){
+#    for (t in 1:3){
+      system(paste0('sbatch -o test.out -t 10:00:00 -n 1 --wrap="Rscript test0613.R alpha0=', 0, ' alpha=', a, ' myseed=', myseeds[i], ' t=', t, '"'))
+#    }
+#  }
+}
